21166c43c943b0b7d2e0d00c6711a57b31853d72
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-07-22 08:48:31 +0100

    delete some unneeded code

--- scripts/empirical-analysis.R
+++ scripts/empirical-analysis.R
@@ -1,6 +1,6 @@
-#
-#
-#
+
+# Roma regressions
+# translated from original STATA code
 
 library(foreign)
 library(estimatr)
@@ -13,7 +13,7 @@ library(reshape2)
 library(psych)
 library(tidyr)
 
-# Read the Stata dataset
+# Read Stata dataset
 data <- haven::read_dta("../data/dataDEF.dta")
 
 country_labels <- names(attributes(data$country)$labels)
@@ -22,14 +22,8 @@ sample_labels <- names(attributes(data$sample)$labels)
 data$country_labels <- country_labels[data$country]
 data$sample_labels <- sample_labels[data$sample + 1]
 
-# First table - Percentage of Roma for each country
-tab <- as.data.frame(table(data$sample, data$country))
-colnames(tab) <- c("sample", "country", "count")
-tab
-
-# Presentation of DEPENDENT VARIABLES.
-
 # 1) Screening initiative
+# 0-4, NA
 data$screening_initiative <- ifelse(data$screening > 0, 1, 0)
 data$screening_initiative <- ifelse(data$screening_initiative != 1, 0, 1)
 
@@ -68,10 +62,6 @@ ggplot(plot_dat, aes(country_labels, prop)) +
 ggsave("../plots/boxplot_healthcare_access.png", width = 8, height = 7)
 
 # 2) Health behavior
-tab_health_behavior <- as.data.frame(table(data$health_behavior))
-colnames(tab_health_behavior) <- c("health_behavior", "count")
-tab_health_behavior
-
 data$health_behavior_roma <- ifelse(data$health_behavior == 1 & data$sample == 1, 1, 0)
 data$health_behavior_roma <- ifelse(data$health_behavior == 0 & data$sample == 1, 0, 1)
 
@@ -109,18 +99,18 @@ ggsave("../plots/boxplot_unmet_need.png", width = 8, height = 7)
 ################
 # summary table
 
-variable_names <- c("discrimination_ethnicity", "age", "no_educ", "female", "afford_n", "health_insurance", "country")
+tab_var_names <- c("discrimination_ethnicity", "age", "no_educ", "female", "afford_n", "health_insurance", "country")
 
 tab_01 <- data |> 
-  select(all_of(c("sample", variable_names))) |> 
+  select(all_of(c("sample", tab_var_names))) |> 
   group_by(sample) |> 
-  summarise(across(all_of(variable_names), \(x) mean(x, na.rm = TRUE))) |> 
+  summarise(across(all_of(tab_var_names), \(x) mean(x, na.rm = TRUE))) |> 
   mutate(sample = as.character(sample)) |> 
   tibble::column_to_rownames(var = "sample")
 
 tab_all <- data |> 
-  select(all_of(c("sample", variable_names))) |> 
-  summarise(across(all_of(variable_names), \(x) mean(x, na.rm = TRUE))) |> 
+  select(all_of(c("sample", tab_var_names))) |> 
+  summarise(across(all_of(tab_var_names), \(x) mean(x, na.rm = TRUE))) |> 
   mutate(sample = "All") |> 
   tibble::column_to_rownames(var = "sample")
 
@@ -134,21 +124,40 @@ summary_tab <-
 
 write.csv(summary_tab, file = "../output/summary_tab.csv")
 
+# alternatively
+###TODO:
+# stargazer(data, type = "text", summary = TRUE)
+
+# this seems to be a nice option
+##TODO: split into Roma/non-Roma columns
+psych::describe(data)
 
 ########################################
 # Presentation of EXPLANATORY VARIABLES
 
 # 1) Community support
+# value           label
+# 1                      A friend
+# 2             A relative/family
+# 3                      Employer
+# 4   A rich man in the community
+# 5 Social assistance institution
+# 6                        A bank
+# 7    A microfinance institution
+# 8                     Local NGO
+# 95                         Other
+# 96                        No one
+# 888998    RF/DK (Refused/Don't Know)
+
 ##TODO: should this be 96: No one?
+## does NA mean No one?
+## if the first one if No one then it doesnt make sense to have anything for the others
+## similarly for second_support
 data$community_support <- ifelse(data$first_support > 0 | data$second_support > 0 | data$third_support > 0, 1, 0)
-data$community_support <- ifelse(data$first_support != 96 | data$second_support != 96 | data$third_support != 96, 1, 0)
+data$community_support <- data$first_support != 96 | data$second_support != 96 | data$third_support != 96
 
 ##TODO: why do this?
 data$community_support_n <- (data$community_support - 0) / 3
-
-tab_community_support_n <- as.data.frame(table(data$community_support_n, data$sample))
-colnames(tab_community_support_n) <- c("community_support_n", "sample", "count")
-tab_community_support_n
 
 # data <- subset(data, select = -c(first_support, second_support, third_support, community_support))
 
@@ -173,81 +182,35 @@ data$stealingfood_acceptance <- ifelse(data$stealingfood_acceptance > 3, NA, dat
 ##Error
 data$stealingfood_acceptance <- ifelse(data$stealingfood_acceptance == 2 | data)
 
-# Generate Table 2: Explanatory Variables
-
-# Perform t-test
-s4 <- t.test(community_support_n ~ sample, data = data)
-s4 <- cbind(s4$estimate[1:2], s4$stderr[1:2])
-
-# Create a data frame for the table
-table_data <- data.frame(
-  Roma = s4[1, 1:2],
-  Non_Roma = s4[2, 1:2]
-)
-
-rownames(table_data) <- c("Community support", "Own norms")
-colnames(table_data) <- c("Mean", "SE")
-
-# Print the table
-stargazer(table_data, title = "Explanatory variables", column.labels = c("Roma", "Non-Roma"), digits = 2, header = TRUE)
-
-# Create interaction terms for explanatory variables
-data$community_support_ni <- data$sample * data$community_support_n
-data$own_norms_ni <- data$sample * data$own_norms_n
-data$discrimination_ethnicity_i <- data$discrimination_ethnicity * data$sample
-
-# Label variables
-label(data$community_support_n) <- "Community support"
-label(data$community_support_ni) <- "Community support x Roma"
-label(data$own_norms_n) <- "Follow own norms"
-label(data$own_norms_ni) <- "Follow own norms x Roma"
-label(data$discrimination_ethnicity) <- "Ethnic discrimination"
-label(data$discrimination_ethnicity_i) <- "Discriminated for ethnicity x Roma"
-
 # Create an asset index
-
-data <- data.frame(
+data_asset <- data.frame(
   radio, tv, bike, car, horse, computer, internet, phone, washingmachine,
   bed_foreach, books30, powergenerator, kitchen, piped, toilet, wastewater,
   bathroom, electricity, heating)
 
-kmo <- KMO(data)
-comp1 <- principal(data)$values[, 1]
+# Measure of Sampling Adequacy
+kmo <- KMO(data_asset)
+comp1 <- principal(data_asset)$values[, 1]
 hist(comp1)
 colnames(comp1) <- "asset_score"
 xtile(asset_index <- comp1, n = 5)
 
-# Label asset index variable
 levels(asset_index) <- c("Poorest", "Poorer", "Middle", "Richer", "Richest")
-label(asset_index) <- "Asset index"
 
-# Create Table 3 "Characteristics of the respondents"
-label(age) <- "Age"
-label(educ_years) <- "Educational years"
-label(unemployed) <- "Unemployed"
-label(food_insecurity) <- "Food insecurity"
-label(health_insurance) <- "Health insurance"
-label(good_health) <- "Self-reported good health"
-label(health_behavior) <- "Avoidance of medical screening"
-label(female) <- "Percentage of females"
-label(afford_n) <- "Economic security"
+data_Roma <- subset(data, sample == 1)
+data_nonRoma <- subset(data, sample == 0)
 
-# Subset the data for different samples
-summary_data1 <- subset(data, sample == 1)
-summary_data0 <- subset(data, sample == 0)
+# summary statistics using stargazer
+stargazer(data_Roma, title = "Sample 1 Characteristics", digits = 2, type = "text")
+stargazer(data_nonRoma, title = "Sample 0 Characteristics", digits = 2, type = "text")
 
-# Print the summary statistics using stargazer
-stargazer(summary_data1, title = "Sample 1 Characteristics", digits = 2, type = "text")
-stargazer(summary_data0, title = "Sample 0 Characteristics", digits = 2, type = "text")
-
+type_neighbourhood <- c("town", "village", "capital", "city", "unregulated_area")
 
 ##############
 # regressions
+##############
 
 # 1) Occurrence of health services
-
-# a) Logistic regression for occurrence of healthcare (Table 4)
-type_neighbourhood <- c("town", "village", "capital", "city", "unregulated_area")
 
 # logistic regression model
 ##TODO: interaction term for sample*community_support?
